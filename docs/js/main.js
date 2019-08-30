12c5926b27eb97fbfdffca6a5bffbefb7911ed1c
Joel Osorio <joelosorio44@gmail.com>
2019-08-29 19:03:49 -0500

    se agrego la api del clarin

--- docs/js/main.js
+++ docs/js/main.js
@@ -8,7 +8,7 @@ window.onload = function(){
 function peticionAPI(){
     let mensaje = new XMLHttpRequest()
 
-    mensaje.open("GET","https://api.sheety.co/5c34824b-0097-4e84-b841-e38fb0fbe1cd")
+    mensaje.open("GET","https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.clarin.com%2Frss%2Fdeportes")
 
     mensaje.send()
 
@@ -26,15 +26,16 @@ function peticionAPI(){
 
 function renderizar(noticias){
     //console.log(noticias)
-    noticias.forEach(
+    noticias.items.forEach(
         function(noticia){
             //console.log("El titulo es: " + noticia.titulo)
             let articuloMatriz = document.querySelector(".noticia")
             let articuloCopia = articuloMatriz.cloneNode(true)
-            articuloCopia.querySelector("h3").innerText = noticia.titulo
-            articuloCopia.querySelector("a").innerText = noticia.autor
-            articuloCopia.querySelector("p:nth-of-type(2)").innerText = noticia.detalle
-            articuloCopia.querySelector("img").src = noticia.imagen
+
+            articuloCopia.querySelector("h3").innerText = noticia.title
+            articuloCopia.querySelector("a").innerText = noticia.author
+            articuloCopia.querySelector("p:nth-of-type(2)").innerText = noticia.description
+            articuloCopia.querySelector("img").src = noticia.enclosure.link
 
             articuloCopia.classList.remove("d-none")
 
